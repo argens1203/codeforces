f14a664b1372e132810a5b5712623cbc5ad0927e
Argens <argens1203@gmail.com>
2024-04-12 23:45:36 +1000

    feat(938): would not work with too much memory usage

--- r938/defense.cpp
+++ r938/defense.cpp
@@ -19,79 +19,10 @@ typedef unordered_map<int, int> umii;
 typedef unsigned long long ull;
 typedef map<pair<int, int>, int> mpii;
 
-// 1 use unsigned long long
-// 2 optimize NcR
-// 3 make sure sum up before averaging to prevent rounding error
-
-umii intoMap(vi v){
-    umii ma;
-    for (auto& i : v){
-        if (ma.find(i) == ma.end()){
-            ma[i] = 1;
-        } else {
-            ma[i]++;
-        }
-    }
-    return ma;
-}
-
-unsigned long long fac(int x, int start = 1){
-    unsigned long long total = 1;
-    for (int i = start; i <= x; i++){
-        total *= i;
-    }
-    return total;
-}
-
-unsigned long long c(int n, int r){
-    if (r < n / 2) return c (n, n - r);
-    return fac(n, r + 1) / fac(n - r);
-}
-
-long long avg(int n, int m){
-    int total = c(n, m);
-
-    unsigned long long digit = (unsigned long long) total * m / n;
-    unsigned long long sum = 0;
-    for (int i = 0; i < n; i++){
-        sum += (unsigned long long) (digit << i);
-    }
-    unsigned long long a = sum / total;
-    return a;
-}
-
-mpii spread(mpii mapp, int w, int h){
-    mpii minusOne;
-    for (auto itr : mapp){
-        int x = itr.first.first;
-        int y = itr.first.second;
-        int value = itr.second;
-        if (x >= 2) minusOne[{x - 1, y}] = max(minusOne[{x - 1,y}], value );
-        if (x <= w - 1) minusOne[{x + 1, y}] = max(minusOne[{x + 1,y}], value);
-        minusOne[{x, y}] = max(minusOne[{x,y}], value);
-        if (y >= 2) minusOne[{x, y - 1}] = max(minusOne[{x,y - 1}], value);
-        if (y <= h - 1) minusOne[{x, y + 1}] = max(minusOne[{x,y + 1}], value);
-    }
-    return minusOne;
-}
-
-ll comb(ll n, int r){
-    return n * (n - 1) / 2;
-}
-
-int gcd(int a, int b){
-    if (b == 0) return a;
-    if (a == b) return a;
-    if (a > b) return gcd(b, a % b);
-    return gcd(b, a);
-}
-
-double dist(pair<int, int> a, pair<int, int> b){
-    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
-}
-
 const int R = 12;
 const int NINF = -2e9;
+
+vector<int>cost (R);
 
 void solve(){
     int n, m, k;
@@ -113,13 +44,6 @@ void solve(){
         --tower[i].second;
     }
     // end of taking input
-
-    vector<int>cost (R);
-    int c = 1;
-    for (int i = 0; i < R; ++i){
-        c *= 3;
-        cost[i] = c;
-    }
 
     auto benefit = [&](int t_idx, int r) -> int {
         int tx = tower[t_idx].first;
@@ -173,6 +97,13 @@ void solve(){
 int main(){
     int t;
     cin >> t;
+
+    int c = 1;
+    for (int i = 0; i < R; ++i){
+        c *= 3;
+        cost[i] = c;
+    }
+
     while (t--){
         solve();
     }
