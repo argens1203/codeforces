584733a6c677f31ca336a7324adc147aabcd2217
Argens <argens1203@gmail.com>
2024-05-02 10:40:08 +1000

    feat(164): ugly

---
+++ r164/ugly.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <cmath>
+#include <set>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+
+typedef long long ll;
+typedef vector<vector<int>> vvi;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef unordered_map<long long, int> umlli;
+typedef unordered_map<int, int> umii;
+typedef unsigned long long ull;
+typedef map<pair<int, int>, int> mpii;
+
+// 1 use unsigned long long
+// 2 optimize NcR
+// 3 make sure sum up before averaging to prevent rounding error
+
+umii intoMap(vi v){
+    umii ma;
+    for (auto& i : v){
+        if (ma.find(i) == ma.end()){
+            ma[i] = 1;
+        } else {
+            ma[i]++;
+        }
+    }
+    return ma;
+}
+
+unsigned long long fac(int x, int start = 1){
+    unsigned long long total = 1;
+    for (int i = start; i <= x; i++){
+        total *= i;
+    }
+    return total;
+}
+
+unsigned long long c(int n, int r){
+    if (r < n / 2) return c (n, n - r);
+    return fac(n, r + 1) / fac(n - r);
+}
+
+long long avg(int n, int m){
+    int total = c(n, m);
+
+    unsigned long long digit = (unsigned long long) total * m / n;
+    unsigned long long sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += (unsigned long long) (digit << i);
+    }
+    unsigned long long a = sum / total;
+    return a;
+}
+
+mpii spread(mpii mapp, int w, int h){
+    mpii minusOne;
+    for (auto itr : mapp){
+        int x = itr.first.first;
+        int y = itr.first.second;
+        int value = itr.second;
+        if (x >= 2) minusOne[{x - 1, y}] = max(minusOne[{x - 1,y}], value );
+        if (x <= w - 1) minusOne[{x + 1, y}] = max(minusOne[{x + 1,y}], value);
+        minusOne[{x, y}] = max(minusOne[{x,y}], value);
+        if (y >= 2) minusOne[{x, y - 1}] = max(minusOne[{x,y - 1}], value);
+        if (y <= h - 1) minusOne[{x, y + 1}] = max(minusOne[{x,y + 1}], value);
+    }
+    return minusOne;
+}
+
+ll comb(ll n, int r){
+    return n * (n - 1) / 2;
+}
+
+int gcd(int a, int b){
+    if (b == 0) return a;
+    if (a == b) return a;
+    if (a > b) return gcd(b, a % b);
+    return gcd(b, a);
+}
+
+double dist(pair<int, int> a, pair<int, int> b){
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+const int R = 12;
+const int NINF = -2e9;
+
+void solve(){
+    int n;
+    cin >>n;
+    vi list(n);
+    for (auto & i : list){
+        cin >> i;
+    }
+
+    int min_count = n;
+    int start = 0;
+    for (int i = 0; i < n; ++i){
+        if (list[i] != list[n-1]){
+            int count = i - start;
+            min_count = min(min_count, count);
+            start = i + 1;
+        }
+    }
+    start = n - 1;
+    for (int i = n - 1; i >= 0; --i){
+        if (list[i] != list[0]){
+            int count = start - i;
+            min_count = min(min_count, count);
+            start = i - 1;
+        }
+    }
+    cout << (min_count == n ? -1 : min_count) << "\n";
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
