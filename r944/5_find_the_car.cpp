23324d721ca628335c55591ab45b7f52dc68e5f8
Argens <argens1203@gmail.com>
2024-05-12 19:52:05 +1000

    wip(944):

---
+++ r944/5_find_the_car.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <cmath>
+#include <set>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+
+typedef long long ll;
+typedef vector<vector<int>> vvi;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef unordered_map<long long, int> umlli;
+typedef unordered_map<int, int> umii;
+typedef unsigned long long ull;
+typedef map<pair<int, int>, int> mpii;
+
+// 1 use unsigned long long
+// 2 optimize NcR
+// 3 make sure sum up before averaging to prevent rounding error
+
+umii intoMap(vi v){
+    umii ma;
+    for (auto& i : v){
+        if (ma.find(i) == ma.end()){
+            ma[i] = 1;
+        } else {
+            ma[i]++;
+        }
+    }
+    return ma;
+}
+
+unsigned long long fac(int x, int start = 1){
+    unsigned long long total = 1;
+    for (int i = start; i <= x; i++){
+        total *= i;
+    }
+    return total;
+}
+
+unsigned long long c(int n, int r){
+    if (r < n / 2) return c (n, n - r);
+    return fac(n, r + 1) / fac(n - r);
+}
+
+long long avg(int n, int m){
+    int total = c(n, m);
+
+    unsigned long long digit = (unsigned long long) total * m / n;
+    unsigned long long sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += (unsigned long long) (digit << i);
+    }
+    unsigned long long a = sum / total;
+    return a;
+}
+
+mpii spread(mpii mapp, int w, int h){
+    mpii minusOne;
+    for (auto itr : mapp){
+        int x = itr.first.first;
+        int y = itr.first.second;
+        int value = itr.second;
+        if (x >= 2) minusOne[{x - 1, y}] = max(minusOne[{x - 1,y}], value );
+        if (x <= w - 1) minusOne[{x + 1, y}] = max(minusOne[{x + 1,y}], value);
+        minusOne[{x, y}] = max(minusOne[{x,y}], value);
+        if (y >= 2) minusOne[{x, y - 1}] = max(minusOne[{x,y - 1}], value);
+        if (y <= h - 1) minusOne[{x, y + 1}] = max(minusOne[{x,y + 1}], value);
+    }
+    return minusOne;
+}
+
+ll comb(ll n, int r){
+    return n * (n - 1) / 2;
+}
+
+int gcd(int a, int b){
+    if (b == 0) return a;
+    if (a == b) return a;
+    if (a > b) return gcd(b, a % b);
+    return gcd(b, a);
+}
+
+double dist(pair<int, int> a, pair<int, int> b){
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+void solve(){
+    int n, k, q;
+    cin >> n >> k >> q;
+    vi a(k + 1); a[0] = 0;
+    vi b(k + 1); b[0] = 0;
+    for (int i = 1; i <= k; ++i) cin >> a[i];
+    for (int i = 1; i <= k; ++i) cin >> b[i];
+
+    vector<double> c(k);
+    for (int i = 1; i <= k; ++i){
+        c[i - 1] =  (b[i] - b[i-1]) * 1.0 /(a[i] - a[i-1]);
+    }
+
+    while (q--){
+        int p; cin >> p;
+        int idx = upper_bound(a.begin(), a.end(), p) - a.begin();
+        int diff = p - a[idx - 1];
+        // int inc = diff * c[idx - 1];
+        int inc = diff * 1.0L * (b[idx] - b[idx-1]) /(a[idx] - a[idx-1]);
+        int ans = inc + b[idx - 1];
+        cout << ans << ' ';
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {solve(); cout << '\n';}
+}
---
+++ r944/7_xour.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <cmath>
+#include <set>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+
+typedef long long ll;
+typedef vector<vector<int>> vvi;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef unordered_map<long long, int> umlli;
+typedef unordered_map<int, int> umii;
+typedef unsigned long long ull;
+typedef map<pair<int, int>, int> mpii;
+
+// 1 use unsigned long long
+// 2 optimize NcR
+// 3 make sure sum up before averaging to prevent rounding error
+
+umii intoMap(vi v){
+    umii ma;
+    for (auto& i : v){
+        if (ma.find(i) == ma.end()){
+            ma[i] = 1;
+        } else {
+            ma[i]++;
+        }
+    }
+    return ma;
+}
+
+unsigned long long fac(int x, int start = 1){
+    unsigned long long total = 1;
+    for (int i = start; i <= x; i++){
+        total *= i;
+    }
+    return total;
+}
+
+unsigned long long c(int n, int r){
+    if (r < n / 2) return c (n, n - r);
+    return fac(n, r + 1) / fac(n - r);
+}
+
+long long avg(int n, int m){
+    int total = c(n, m);
+
+    unsigned long long digit = (unsigned long long) total * m / n;
+    unsigned long long sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += (unsigned long long) (digit << i);
+    }
+    unsigned long long a = sum / total;
+    return a;
+}
+
+mpii spread(mpii mapp, int w, int h){
+    mpii minusOne;
+    for (auto itr : mapp){
+        int x = itr.first.first;
+        int y = itr.first.second;
+        int value = itr.second;
+        if (x >= 2) minusOne[{x - 1, y}] = max(minusOne[{x - 1,y}], value );
+        if (x <= w - 1) minusOne[{x + 1, y}] = max(minusOne[{x + 1,y}], value);
+        minusOne[{x, y}] = max(minusOne[{x,y}], value);
+        if (y >= 2) minusOne[{x, y - 1}] = max(minusOne[{x,y - 1}], value);
+        if (y <= h - 1) minusOne[{x, y + 1}] = max(minusOne[{x,y + 1}], value);
+    }
+    return minusOne;
+}
+
+ll comb(ll n, int r){
+    return n * (n - 1) / 2;
+}
+
+int gcd(int a, int b){
+    if (b == 0) return a;
+    if (a == b) return a;
+    if (a > b) return gcd(b, a % b);
+    return gcd(b, a);
+}
+
+double dist(pair<int, int> a, pair<int, int> b){
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+void solve(){
+    int n; cin >> n;
+    vi a(n);
+    map<int, vector<int>> indices;
+    for (int i = 0; i < n; ++i){
+        cin >> a[i];
+        if (indices.find(a[i] / 4) == indices.end()){
+            indices[a[i] / 4] = vector<int>();
+        }
+        indices[a[i] / 4].push_back(a[i]);
+    }
+
+    for (auto itr = indices.begin(); itr != indices.end(); itr++){
+        sort(itr -> second.begin(), itr -> second.end());
+    }
+    for (int i : a){
+        cout << indices[i / 4].front() << ' ';
+        indices[i / 4].erase(indices[i/4].begin());
+    }
+    cout << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
